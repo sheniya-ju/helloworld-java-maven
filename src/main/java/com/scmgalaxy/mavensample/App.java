861a77fc91be8bc9b3a96d76697b9f0382e999f1
Sheniya <145789837+sheniya-ju@users.noreply.github.com>
2025-05-10 13:47:10 +0530

    Update App.java

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -5,9 +5,10 @@ package com.scmgalaxy.mavensample;
  *
  */
 public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World RefX! MAH-2--d" );
+{ public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
     }
 }
